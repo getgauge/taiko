6c3a4e3b58c0431834ab532b7bf389b52a0a2285
nehashri <nehashrl@thoughtworks.com>
2018-07-17 10:45:34 +0530

    fixes publish nightly script

--- publishNightly.js
+++ publishNightly.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var { exec } = require('child_process');
+var { execSync } = require('child_process');
 var date = new Date();
 
 var p = JSON.parse(fs.readFileSync('package.json'));
@@ -8,12 +8,5 @@ p.version = p.version + '-nightly.' + date.getFullYear() + m + date.getDate();
 
 fs.writeFileSync("package.json", JSON.stringify(p));
 
-exec('npm publish --tag nightly', (err, stdout, stderr) => {
-    if (err) {
-        console.log('Could not publish. Error : ', err);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-});
+execSync('npm publish --tag nightly');
 
