bcccb404f6a76c0dc6a3a9ef70427ae7f891e341
BugDiver <vinaysh@thoughtworks.com>
2018-07-26 17:32:40 +0530

    Merge landing page with api. Make toc scrolable #78

--- docs/setup.js
+++ docs/setup.js
@@ -2,11 +2,17 @@ const fs = require('fs');
 const mds = require('markdown-styles');
 const { metadata } = require('../lib/taiko');
 
+const content = `
+title: Taiko
+toc: ${JSON.stringify(metadata)}
+---
+` + fs.readFileSync('docs/index.md', 'utf-8');
+fs.writeFileSync('docs/index.md', content, 'utf-8');
+
 mds.render(mds.resolveArgs({
-    input: 'docs/api.md',
+    input: 'docs/index.md',
     output: 'docs/site',
     layout: 'docs/layout',
-}), function() {
-    fs.copyFileSync('docs/index.html', 'docs/site/index.html')
+}), function () {
     console.log('All done!');
 });
