cac640080d3832453da6ef98292755974747581f
NivedhaSenthil <nivedhasenthil@gmail.com>
2018-07-26 12:33:56 +0530

    #75 Append args if present

--- taiko.js
+++ taiko.js
@@ -35,9 +35,10 @@ function runFile(file) {
         realFuncs[func] = taiko[func];
         if (realFuncs[func].constructor.name === 'AsyncFunction') global[func] = async function() {
             let res,args = arguments;
-            if(func === 'openBrowser' && observeAgrv.some((val) => argv.includes(val))) args = [{headless: false}];
+            if(func === 'openBrowser' && observeAgrv.some((val) => argv.includes(val))) 
+                if (args['0']) args['0'].headless = false;
+                else args = [{headless:false}] ;
             res = await realFuncs[func].apply(this, args);
-           
             if (res.description) {
                 res.description = symbols.pass + res.description;
                 console.log(removeQuotes(util.inspect(res.description, { colors: true }), res.description));
