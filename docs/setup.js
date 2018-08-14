dc860d5f143b7ed12f8d4616f9248d2b1e33a229
BugDiver <vinaysh@thoughtworks.com>
2018-08-14 16:25:14 +0530

    Addeed old docs layout.

--- docs/setup.js
+++ docs/setup.js
@@ -9,10 +9,22 @@ toc: ${JSON.stringify(metadata)}
 ` + fs.readFileSync('docs/index.md', 'utf-8');
 fs.writeFileSync('docs/index.md', content, 'utf-8');
 
-mds.render(mds.resolveArgs({
-    input: 'docs/index.md',
-    output: 'docs/site',
-    layout: 'docs/layout',
-}), function () {
-    console.log('All done!');
-});
+function generateDoc(layout) {
+    mds.render(mds.resolveArgs({
+        input: 'docs/index.md',
+        output: 'docs/site',
+        layout: layout,
+    }), function () {
+        console.log('All done!');
+    });
+}
+
+function main(arg) {
+    if (arg === '--new') {
+        generateDoc('docs/layout');
+    }else{
+        generateDoc('docs/layoutOld');
+    }
+}
+
+main(process.argv[2]);
