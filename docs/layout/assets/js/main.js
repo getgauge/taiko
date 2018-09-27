f4fc9c8f9f4b540df1652805212d246146f8227a
Chandu199 <n.chandrakanth0@gmail.com>
2018-09-27 11:26:12 +0530

    integrated algolia search

--- docs/layout/assets/js/main.js
+++ docs/layout/assets/js/main.js
@@ -29,4 +29,12 @@ $(document).ready(function() {
 
   }).trigger('resize');
 
+
+  docsearch({ 
+    apiKey: 'b36d75c493b44a8f2605db66708a283b', 
+    indexName: 'taiko', 
+    inputSelector: '#search', 
+    debug: false // Set debug to true if you want to inspect the dropdown 
+    }); 
+
 });
