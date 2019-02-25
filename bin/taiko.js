a7d986493ec294d1628b67b4baa7d4725b219132
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-02-25 15:35:59 +0530

    Updated usages text. #370

--- bin/taiko.js
+++ bin/taiko.js
@@ -73,14 +73,14 @@ function setupEmulateDevice(device) {
         process.env['TAIKO_EMULATE_DEVICE'] = device;
     else {
         console.log(`Invalid value ${device} for --emulate-device`);
-        console.log(`Available devices ${Object.keys(devices).join(', ')}`);
+        console.log(`Available devices: ${Object.keys(devices).join(', ')}`);
         process.exit(1);
     }
 }
 program
     .version(printVersion(), '-v, --version')
-    .usage(`<file> [options]
-       taiko [options]`)
+    .usage(`[options]
+       taiko <file> [options]`)
     .option(
         '-o, --observe [observeTime]',
         'enables headful mode and runs script with 3000ms delay by default\n\t\t\t\toptionally takes observeTime in millisecond eg: --observe 5000',
@@ -90,7 +90,7 @@ program
         '--slow-mod [observeTime]',
         'similar to --observe option'
     )
-    .option('--emulate-device <device>', 'Allows to simulate device viewport. Visit https://github.com/getgauge/taiko/blob/master/lib/device.js for all the available options', setupEmulateDevice)
+    .option('--emulate-device <device>', 'Allows to simulate device viewport. Visit https://github.com/getgauge/taiko/blob/master/lib/device.js for all the available devices', setupEmulateDevice)
     .action(function () {
         if (!isTaikoRunner(program.rawArgs[1])) {
             module.exports = taiko;
