8f5282d47091242305e823cb1fb2426b75c1c8b2
sriv <srikanth.ddit@gmail.com>
2019-04-08 15:50:09 +0530

    capture screencast for file upload

--- examples/01-file_upload.js
+++ examples/01-file_upload.js
@@ -1,17 +1,23 @@
-const { openBrowser, goto, fileField, button, above, attach, click, text, closeBrowser } = require('taiko')
+const { openBrowser, goto, fileField, button, above, attach, click, text, closeBrowser, loadPlugin} = require('taiko')
     , path = require('path')
+    , {ID, clientHandler, startScreencast, stopScreencast} = require('taiko-screencast')
     , expect = require('chai').expect;
+
+loadPlugin(ID, clientHandler);
 
 (async () => {
     try {
         await openBrowser();
+        await startScreencast(path.join('captures', 'file-upload', 'file-upload.gif'))
         await goto('http://localhost:3000/upload');
         await attach(path.join(__dirname, 'data', 'foo.txt'), fileField(above(button('Upload'))));
         await click('Upload');
-        expect(await text('file uploaded!').exists()).to.be.true;
-    } catch (e) {
+        var exists = await text('file uploaded!').exists();
+        expect(exists).to.be.true;
+} catch (e) {
         console.error(e);
     } finally {
+        await stopScreencast();
         await closeBrowser();
     }
 })();
