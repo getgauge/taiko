9b73955fda292d2f36eeecddf037f1da31178e98
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-04-22 12:00:06 +0530

    Moved responsibility of determining the value for waitForNavigation to config module. #328

--- test/unit-tests/config.test.js
+++ test/unit-tests/config.test.js
@@ -1,39 +1,70 @@
-const { setConfig, config } = require('../../lib/config');
+const { setConfig, config, determineWaitForNavigation } = require('../../lib/config');
 const originalConfig = Object.assign({}, config);
 
 describe('Config tests', () => {
 
-    describe('For invalid config name', () => {
+    describe('Test setConfig', () => {
 
-        test('shoud throw exception', () => {
-            let allowedConfig = 'navigationTimeout, observeTime, retryInterval, retryTimeout, waitForNavigation';
-            let expectedMessage = `Invalid config invalidConfig. Allowed configs are ${allowedConfig}`;
-            expect(() => setConfig({ invalidConfig: true })).toThrowError(new RegExp(`^${expectedMessage}$`));
+        describe('For invalid config name', () => {
+
+            test('shoud throw exception', () => {
+                let allowedConfig = 'navigationTimeout, observeTime, retryInterval, retryTimeout, waitForNavigation';
+                let expectedMessage = `Invalid config invalidConfig. Allowed configs are ${allowedConfig}`;
+                expect(() => setConfig({ invalidConfig: true })).toThrowError(new RegExp(`^${expectedMessage}$`));
+            });
+
         });
 
+        describe('For valid config name', () => {
+
+            describe('When valid config value is provided', () => {
+
+                test('should update the config', () => {
+                    const newConfig = {
+                        navigationTimeout: 2,
+                        observeTime: 2,
+                        retryInterval: 2,
+                        retryTimeout: 2,
+                        waitForNavigation: false
+                    };
+                    expect(config).not.toEqual(newConfig);
+
+                    setConfig(newConfig);
+                    expect(config).toEqual(newConfig);
+                });
+
+            });
+
+            describe('When invalid config value is provided', () => {
+
+                test('should throw error', () => {
+                    const expectedMessage = new RegExp('Invalid value for navigationTimeout. Expected number received string');
+                    expect(() => setConfig({ navigationTimeout: 'invalid config value' })).toThrowError(expectedMessage);
+                });
+
+            });
+        });
     });
 
-    describe('For valid config name', () => {
+    describe('Test deriveNavigationTimeout', () => {
 
-        describe('When valid config value is provided', () => {
-
-            test('should update the config', () => {
-                const newConfig = { navigationTimeout: 2, observeTime: 2, retryInterval: 2, retryTimeout: 2, waitForNavigation: false };
-                expect(config).not.toEqual(newConfig);
-
-                setConfig(newConfig);
-                expect(config).toEqual(newConfig);
+        describe('For undefind or null value', () => {
+            test('should return default value when provided value is undefind', () => {
+                let actualValue = determineWaitForNavigation();
+                expect(actualValue).toBeTruthy();
             });
 
+            test('should return default value when provided value is null', () => {
+                let actualValue = determineWaitForNavigation(null);
+                expect(actualValue).toBeTruthy();
+            });
         });
 
-        describe('When invalid config value is provided', () => {
-
-            test('should throw error', () => {
-                const expectedMessage = new RegExp('Invalid value for navigationTimeout. Expected number received string');
-                expect(() => setConfig({ navigationTimeout: 'invalid config value' })).toThrowError(expectedMessage);
+        describe('For correct value', () => {
+            test('should return rovided value', () => {
+                let actualValue = determineWaitForNavigation(false);
+                expect(actualValue).toBeFalsy();
             });
-
         });
     });
 
