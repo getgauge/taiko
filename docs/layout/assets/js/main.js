4d331b419be798616f17a5538a6d23f670b48f37
Subham Singh <s.chauhan5505@gmail.com>
2019-05-26 18:11:59 +0530

    Add tag as environment variable, #546

--- docs/layout/assets/js/main.js
+++ docs/layout/assets/js/main.js
@@ -152,11 +152,12 @@ function smoothScroll() {
 }
 
 function alogoliaSearch() {
+  let tag = process.env.TAG;
   docsearch({ 
     apiKey: 'b36d75c493b44a8f2605db66708a283b', 
     indexName: 'taiko', 
     inputSelector: '#search', 
-    algoliaOptions: { 'facetFilters': ['tags:preview'] },
+    algoliaOptions: { 'facetFilters': [`tags:${tag}`] },
     debug: false // Set debug to true if you want to inspect the dropdown 
     }); 
 }
