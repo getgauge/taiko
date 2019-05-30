b207ee590084935ae2de2fbe780afeae46184a1a
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-05-30 19:01:57 +0530

    Autoload plugin : #562
           - Refactored plugins test to use mocha and rewire

--- test/unit-tests/plugins.test.js
+++ test/unit-tests/plugins.test.js
@@ -1,47 +1,57 @@
-const fs = require('fs');
-const { getPlugins } = require('../../lib/plugins');
-jest.mock('fs');
+const expect = require('chai').expect;
+const rewire = require('rewire');
+const PLUGINS = rewire('../../lib/plugins');
+
+function mockReadFileSyncWith (content) {
+    var fsMock = {
+        readFileSync: function ( ) {
+            return content;
+        }
+    };
+    PLUGINS.__set__('fs', fsMock);
+}
+
 describe('GetPlugins', () => {
     describe('With ENV variable', () => {
         afterEach(() => {
             delete process.env.TAIKO_PLUGIN;
         });
-        test('should give plugin name from ENV variable', () => {
+        it('should give plugin name from ENV variable', () => {
             process.env.TAIKO_PLUGIN = 'some-plugin';
-            expect(getPlugins()).toEqual(['taiko-some-plugin']);
+            expect(PLUGINS.getPlugins()).to.deep.equal(['taiko-some-plugin']);
         });
 
-        test('should give plugin names from ENV variable', () => {
+        it('should give plugin names from ENV variable', () => {
             process.env.TAIKO_PLUGIN = 'plugin-1 ,plugin-2, plugin-3';
-            expect(getPlugins()).toEqual(['taiko-plugin-1', 'taiko-plugin-2', 'taiko-plugin-3']);
+            expect(PLUGINS.getPlugins()).to.deep.equal(['taiko-plugin-1', 'taiko-plugin-2', 'taiko-plugin-3']);
         });
     });
 
     describe('Get plugins from package.json', () => {
-        test('should give plugin name from dependencies', () => {
-            fs.readFileSync.mockReturnValue(`{
+        it('should give plugin name from dependencies', () => {
+            mockReadFileSyncWith(`{
                 "dependencies": {
                     "taiko-dep-plugin-1": "v2.4.5",
                     "taiko-dep-plugin-2": "v1.4.5",
                     "some-other-module" : "v1.0.1"
                 }
             }`);
-            expect(getPlugins()).toEqual(['taiko-dep-plugin-1', 'taiko-dep-plugin-2']);
+            expect(PLUGINS.getPlugins()).to.deep.equal(['taiko-dep-plugin-1', 'taiko-dep-plugin-2']);
         });
 
-        test('should give plugin name from devDependencies', () => {
-            fs.readFileSync.mockReturnValue(`{
+        it('should give plugin name from devDependencies', () => {
+            mockReadFileSyncWith(`{
                 "devDependencies": {
                     "taiko-devDep-plugin-1": "v2.4.5",
                     "taiko-devDep-plugin-2": "v1.4.5",
                     "some-other-module" : "v1.0.1"
                 }
             }`);
-            expect(getPlugins()).toEqual(['taiko-devDep-plugin-1', 'taiko-devDep-plugin-2']);
+            expect(PLUGINS.getPlugins()).to.deep.equal(['taiko-devDep-plugin-1', 'taiko-devDep-plugin-2']);
         });
 
-        test('should give plugin name from both devDependencies and dependencies', () => {
-            fs.readFileSync.mockReturnValue(`{
+        it('should give plugin name from both devDependencies and dependencies', () => {
+            mockReadFileSyncWith(`{
                 "dependencies": {
                     "taiko-dep-plugin-1": "v2.4.5",
                     "taiko-dep-plugin-2": "v1.4.5",
@@ -54,7 +64,7 @@ describe('GetPlugins', () => {
                 }
             }`);
             const expectedPluginNames = ['taiko-dep-plugin-1', 'taiko-dep-plugin-2', 'taiko-devDep-plugin-1', 'taiko-devDep-plugin-2'];
-            expect(getPlugins()).toEqual(expectedPluginNames);
+            expect(PLUGINS.getPlugins()).to.deep.equal(expectedPluginNames);
         });
     });
 });
