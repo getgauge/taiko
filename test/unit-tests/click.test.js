7be69ced71a9f55d7372c0f9464d45f399590e70
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-05-31 14:51:16 +0530

    Added test for click API, #580
        - Added test for clicking on text accross element.
        - Added test for clicking on text nodes.
        - Added test for clicking on element with given text as value.
        - Added test for clicking on element with given text as type.

--- test/unit-tests/click.test.js
+++ test/unit-tests/click.test.js
@@ -1,22 +1,35 @@
 const expect = require('chai').expect;
-let { openBrowser, click, closeBrowser, goto, text } = require('../../lib/taiko');
+let { openBrowser, click, closeBrowser, goto, text, below } = require('../../lib/taiko');
 let { createHtml, removeFile, openBrowserArgs } = require('./test-util');
 const test_name = 'Click';
 
-describe(test_name, () => {
+describe.only(test_name, () => {
     let filePath;
     before(async () => {
         let innerHtml = `
+            <span>Click with proximity</span>
+            <div>
+                <span onclick="displayText('Click works with text nodes.')">Click on text node</span>
+                <span>Click with proximity</span>
+            </div>
+            <span>Proximity marker</span><br/>
+            <span onclick="displayText('Click works with proximity selector.')">Click with proximity</span>
+            <input onclick="displayText('Click works with text as value.')" value="Text as value"/>
+            <input onclick="displayText('Click works with text as type.')" type="Text as type"/>
+            <div onclick="displayText('Click works with text accross element.')">
+                Text <span>accross</span> elements
+            </div>
             <script type="text/javascript">
-                function displayText() {
-                    document.getElementById('root').innerText = "Click works with auto scroll."
+                function displayText(text) {
+                    document.getElementById('root').innerText = text
                 }
             </script>
             <div style="height:1500px"></div>
             <div id="root" style="background:red;"></div>
-            <span onclick="displayText()">Show Message</span>
+            <span onclick="displayText('Click works with auto scroll.')">Show Message</span>
             `;
         filePath = createHtml(innerHtml, test_name);
+        // await openBrowser({args:openBrowserArgs.args, headless:false/*, observe:true, observeTime:5000*/});
         await openBrowser(openBrowserArgs);
         await goto(filePath);
     });
@@ -32,4 +45,40 @@ describe(test_name, () => {
             expect(await text('Click works with auto scroll.').exists()).to.be.true;
         });
     });
+
+    describe('With text nodes', () => {
+        it('should click', async () => {
+            await click('on text');
+            expect(await text('Click works with text nodes.').exists()).to.be.true;
+        });
+    });
+
+    describe('With proximity selector', () => {
+        xit('should click', async () => {
+            await click('Click with proximity', below('Proximity marker'));
+            expect(await text('Click works with proximity selector.').exists()).to.be.true;
+        });
+    });
+
+    describe('Text accross element', () => {
+        it('should click', async () => {
+            await click('Text accross elements');
+            expect(await text('Click works with text accross element.').exists()).to.be.true;
+        });
+    });
+
+    describe('Text as value', () => {
+        it('should click', async () => {
+            await click('Text as value');
+            expect(await text('Click works with text as value.').exists()).to.be.true;
+        });
+    });
+
+    describe('Text as type', () => {
+        it('should click', async () => {
+            await click('Text as type');
+            expect(await text('Click works with text as type.').exists()).to.be.true;
+        });
+    });
+
 });
