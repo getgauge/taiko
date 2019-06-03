a050970183583f54814a1d95056e306857345874
Subham Singh <s.chauhan5505@gmail.com>
2019-06-03 14:33:04 +0530

    Add tests for $  #581

---
+++ test/unit-tests/$.test.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect;
+let { openBrowser, closeBrowser, goto, $, text, above } = require('../../lib/taiko');
+let { createHtml, removeFile, openBrowserArgs } = require('./test-util');
+const test_name = '$';
+
+describe(test_name, () => {
+    let filePath;
+    before(async () => {
+        let innerHtml = `
+        <div class="test">
+            <p id="foo">taiko</p>
+            <p>demo</p>
+        </div>
+            `;
+        filePath = createHtml(innerHtml, test_name);
+        await openBrowser(openBrowserArgs);
+        await goto(filePath);
+    });
+
+    after(async () => {
+        await closeBrowser();
+        removeFile(filePath);
+    });
+
+    describe('text with xpath', () => {
+        it('should find text with xpath', async () => {
+            expect(await $('//*[text()=\'taiko\']').exists()).to.be.true;
+        });
+        it('should find text with selectors', async () => {
+            expect(await $('#foo').exists()).to.be.true;
+            expect(await $('.test').exists()).to.be.true;
+        });
+        it('should find text with proximity selector', async () => {
+            expect(await text('taiko', above($('//*[text()=\'demo\']'))).exists()).to.be.true;
+        });
+    });
+});
