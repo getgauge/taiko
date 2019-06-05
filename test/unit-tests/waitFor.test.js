397ae26d9303bcbfa9a8494c4809a307c623d622
Subham Singh <s.chauhan5505@gmail.com>
2019-06-05 14:15:57 +0530

    Handle timeout for non existing elements, #531

--- test/unit-tests/waitFor.test.js
+++ test/unit-tests/waitFor.test.js
@@ -1,10 +1,14 @@
-const expect = require('chai').expect;
-let { openBrowser, goto, closeBrowser, waitFor, intervalSecs, text } = require('../../lib/taiko');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+let { openBrowser, goto, closeBrowser, waitFor, intervalSecs } = require('../../lib/taiko');
 let { createHtml, removeFile, openBrowserArgs } = require('./test-util');
 const test_name = 'waitFor';
 
+chai.use(chaiAsPromised);
 
-describe(test_name, () => {
+describe(test_name, function() {
+    this.timeout(12000);
     let filePath;
     before(async () => {
         let innerHtml =
@@ -20,40 +24,39 @@ describe(test_name, () => {
         await closeBrowser();
         removeFile(filePath);
     });
+
+    describe('waitFor test without time', () => {
+        it('should not wait if time is not given', async () => {
+            expect(waitFor()).not.to.eventually.be.rejected;
+        });
+    });
+
     describe('waitFor test with only time', () => {
         it('should wait just for given time', async () => {
-            await waitFor(3000);
-            expect(await text('beautiful').exists()).to.be.true;
-        }).timeout(4000);
+            expect(waitFor(intervalSecs(3))).not.to.eventually.be.rejected;
+        });
     });
 
     describe('waitFor test with only element', () => {
         it('should wait for element for default timeout', async () => {
-            await waitFor('beautiful');
-            expect(await text('beautiful').exists()).to.be.true;
+            expect(waitFor('beautiful')).not.to.eventually.be.rejected;
         });
 
-        xit('should timeout if element is not there', async () => {
-            const expectedMessage = new RegExp('waiting failed: retryTimeout 10000ms exceeded');
-            expect(await waitFor('something that is not there')).to.throw(expectedMessage);
-        }).timeout(12000);
+        it('should timeout if element is not there', async () => {
+            const expectedMessage = new RegExp('Waiting Failed: Element \'something that is not there\' not found within 10000 ms');
+            expect(waitFor('something that is not there')).to.eventually.be.rejectedWith(expectedMessage);
+        });
 
     });
 
     describe('waitFor test with element and time', () => {
         it('should wait for element with given time ', async () => {
-            await waitFor('beautiful', 1000);
-            expect(await text('beautiful').exists()).to.be.true;
+            expect(waitFor('Patience', intervalSecs(4))).not.to.eventually.be.rejected;
         });
 
-        it('should return the element if element is there', async () => {
-            await waitFor('beautiful', 3000);
-            expect(await text('beautiful').exists()).to.be.true;
-        }).timeout(5000);
-
-        xit('should wait for element for the given time', async () => {
-            const expectedMessage = new RegExp('waiting failed: retryTimeout 5000ms exceeded');
-            expect(await waitFor('something that is not there', intervalSecs(5))).to.throw(expectedMessage);
+        it('should wait for element for the given time', async () => {
+            const expectedMessage = new RegExp('Waiting Failed: Element \'something that is not there\' not found within 3000 ms');
+            expect(waitFor('something that is not there', intervalSecs(3))).to.eventually.be.rejectedWith(expectedMessage);
         });
     });
 });
