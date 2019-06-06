67ac57fa498ccb19d7ba7bbd9fd19e90337a6fce
BugDiver <vinaysh@thoughtworks.com>
2019-06-06 13:55:27 +0530

    Added tests for closeTab and targetHandler.

---
+++ test/unit-tests/closeTab.test.js
@@ -0,0 +1,69 @@
+const expect = require('chai').expect;
+const { EventEmitter } = require('events');
+const rewire = require('rewire');
+const taiko = rewire('../../lib/taiko');
+const targetHandler = rewire('../../lib/targetHandler');
+
+describe('closeTab', () => {
+    let _targets = { matching: [], others: [] };
+    let currentTarget;
+    let descEmmitter = new EventEmitter();
+
+    before(() => {
+        let mockCri = {
+            Close: async function () { }
+        };
+
+        let mockHandler = {
+            getCriTargets: () => { return _targets; },
+            constructCriTarget: (arg) => { return arg; }
+        };
+
+        targetHandler.__set__('cri', mockCri);
+        taiko.__set__('targetHandler', mockHandler);
+        taiko.__set__('descEvent', descEmmitter);
+        taiko.__set__('cri', mockCri);
+        taiko.__set__('connect_to_cri', async (target) => { currentTarget = target; });
+        taiko.__set__('dom', { getDocument: async () => { } });
+    });
+
+    beforeEach(() => {
+        descEmmitter.removeAllListeners();
+        taiko.__set__('_client', new EventEmitter());
+        _targets = { matching: [], others: [] };
+    });
+
+    it('should close the browser if there are no tabs to reconnect', async () => {
+        taiko.__set__('_closeBrowser', () => { });
+        descEmmitter.once('success', (message) => {
+            expect(message).to.be.eql('Closing last target and browser.');
+        });
+        await taiko.closeTab();
+    });
+
+    it('should close the current tab and switch to last active target if no url given', async () => {
+        _targets.matching.push({ id: '1', type: 'page', url: 'https://flipkart.com' });
+        _targets.others.push({ id: '2', type: 'page', url: 'https://flipkart.com' });
+        _targets.others.push({ id: '3', type: 'page', url: 'https://amazon.com' });
+
+        descEmmitter.on('success', (message) => {
+            expect(message).to.be.eql('Closed current tab with url https://flipkart.com');
+        });
+        await taiko.closeTab();
+        expect(currentTarget.url).to.be.eql('https://flipkart.com');
+    });
+
+    it('should close the all mathicng tabs  with given url switch to last active', async () => {
+        _targets.matching.push({ id: '1', type: 'page', url: 'https://flipkart.com' });
+        _targets.others.push({ id: '2', type: 'page', url: 'https://amazon.com' });
+        _targets.matching.push({ id: '3', type: 'page', url: 'https://flipkart.com' });
+
+        descEmmitter.on('success', (message) => {
+            expect(message).to.be.eql('Closed all tabs with url https://flipkart.com');
+        });
+        await taiko.closeTab('https://flipkart.com');
+        expect(currentTarget.url).to.be.eql('https://amazon.com');
+
+    });
+
+});
---
+++ test/unit-tests/targetHandler.test.js
@@ -0,0 +1,60 @@
+const expect = require('chai').expect;
+let rewire = require('rewire');
+const targetHandler = rewire('../../lib/targetHandler');
+
+describe('TargetHandler', () => {
+
+    describe('.getCriTargets', () => {
+        let _targets = [];
+
+        before(() => {
+            let mockCri = {
+                List: function () {
+                    return _targets.reverse();
+                }
+            };
+            targetHandler.__set__('cri', mockCri);
+        });
+
+        beforeEach(() => {
+            _targets = [];
+        });
+        it('should give current tab as matching if no url given', async () => {
+            _targets.push({ id: '1', type: 'page' });
+            _targets.push({ id: '2', type: 'page' });
+            let targets = await targetHandler.getCriTargets();
+            expect(targets.matching.length).to.be.equal(1);
+            expect(targets.matching[0].id).to.be.equal('2');
+            expect(targets.others.length).to.be.equal(1);
+            expect(targets.others[0].id).to.be.equal('1');
+        });
+
+
+        it('should give all matching tabs if url is given', async () => {
+            _targets.push({ id: '1', type: 'page', url: 'https://flipkart.com' });
+            _targets.push({ id: '2', type: 'page', url: 'https://amazon.com' });
+            _targets.push({ id: '3', type: 'page', url: 'https://flipkart.com' });
+
+            let targets = await targetHandler.getCriTargets('flipkart.com');
+
+            expect(targets.matching.length).to.be.equal(2);
+            expect(targets.matching[0].id).to.be.equal('3');
+            expect(targets.matching[0].url).to.be.equal('https://flipkart.com');
+            expect(targets.others.length).to.be.equal(1);
+            expect(targets.others[0].url).to.be.equal('https://amazon.com');
+        });
+
+        it('should give no matching tabs if given url does not exists', async () => {
+            _targets.push({ id: '1', type: 'page', url: 'https://flipkart.com' });
+            _targets.push({ id: '2', type: 'page', url: 'https://amazon.com' });
+            _targets.push({ id: '3', type: 'page', url: 'https://flipkart.com' });
+
+            let targets = await targetHandler.getCriTargets('gauge.org');
+
+            expect(targets.matching.length).to.be.equal(0);
+            expect(targets.others.length).to.be.equal(3);
+        });
+    });
+
+});
+
