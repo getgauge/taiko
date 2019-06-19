53b9e195109fe723616fc45e8a368da2596a97fe
Nehashri <neha.nehe@gmail.com>
2019-06-19 12:15:09 +0530

    fixes examples to throw error when tests fail

--- examples/runner.js
+++ examples/runner.js
@@ -10,24 +10,35 @@ var server = app.listen(3000, async () => {
         var a = args.includes('--observe') ? f + ' --observe': f;
         var env = process.env;
         env["SCREENCAST_ENABLED"] = args.includes('--screencast');
-        var p = cp.exec('taiko ' + a, {env: env});
+        var p = cp.exec('taiko ' + a, {env: env}, (error) => {
+            if (error) {
+                reject(error);
+            }
+            resolve();
+        });
         p.stdout.pipe(process.stdout);
         p.stderr.pipe(process.stderr);
-        p.on('exit', resolve);
-        p.on('error', reject);
     });
     
     var prefix = args.filter(a => !['--observe', '--screencast'].includes(a))[2] || '';
     var examples = glob.sync('*.js')
         .filter(f => __filename !== path.resolve(f) && 'browserLauncher.js' !== f && f.startsWith(prefix))
         .map(f => {return {file:f, task: () => run(f)};});
+    var failed = false;
     for (let example of examples){
         try {
             await example.task();
         } catch (e) {
-            console.error('Unable to run ' + example.file + '\n' + e);
+            console.error('Unable to run ' + example.file);
+            failed = true;
         }
     }
     console.log('Shutting down the Internet Express');
-    server.close();
+    server.close((e) => {
+        if (e) {
+            console.error('Failed to close the Internet Express', e);
+            failed = true;
+        }
+    });
+    if(failed) process.exit(1);
 });
