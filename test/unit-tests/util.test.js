5ab6ee392b56dd75ed5d36888e08e4880aad7e6d
Ankon Chakraborty <44019225+luciferankon@users.noreply.github.com>
2019-06-27 11:40:44 +0530

    Extracted function and removed regex matching (#656)
    * fix to switchTo tab is not working #621
    
    * used url module to parse the url #621
    
    * parsed url using node url module. Added util function to trim specific character #621
    
    * Change trimChar to trimCharLeft and extracted function isMatchingUrl

--- test/unit-tests/util.test.js
+++ test/unit-tests/util.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
-const { trimChar } = require('../../lib/util');
+const { trimCharLeft } = require('../../lib/util');
 const test_name = 'util';
 
 describe(test_name, () => {
     describe('trim Char()', () => {
         it('should trim the char specified from the string', async () => {
-            const actual = trimChar('|foo|','|');
+            const actual = trimCharLeft('|foo','|');
             const expected = 'foo';
             expect(actual).to.be.equal(expected);
         });
