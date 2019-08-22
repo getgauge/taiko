0a4927afea33e23ab3c78b05a81e5911ff933957
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-08-22 11:08:29 +0530

    evaluate API should work with async function. #760
         - Added test for evaluate API.
         - Added support for async function in evaluate API.

---
+++ test/unit-tests/evaluate.test.js
@@ -0,0 +1,49 @@
+const { createHtml, openBrowserArgs, removeFile } = require('./test-util');
+const { openBrowser, goto, evaluate, closeBrowser } = require('../../lib/taiko');
+const expect = require('chai').expect;
+const testName = 'Evaluate';
+
+describe.only(testName, () => {
+    let filePath;
+    before(async () => {
+        let innerHtml =
+            `<section class="header">
+                    <h1>${testName} tests</h1>
+                 </section>
+                `;
+        filePath = createHtml(innerHtml, testName);
+        await openBrowser(openBrowserArgs);
+        await goto(filePath);
+    });
+
+    after(async () => {
+        await closeBrowser();
+        removeFile(filePath);
+    });
+
+    describe('without selector', () => {
+        it('should pass root html element to the function to be evaluated', async () => {
+            const evaluationResult = await evaluate((rootElement) => {
+                return rootElement.outerHTML;
+            });
+            expect(evaluationResult).to.match(/^<html>/);
+            expect(evaluationResult).to.match(/<\/html>$/);
+        });
+
+        it('should return the result of the evaluation', async () => {
+            const evaluationResult = await evaluate(() => {
+                return document.title;
+            });
+            expect(evaluationResult).to.equal(testName);
+        });
+
+        it('should return the result of evaluation with async function', async () => {
+            const evaluationResult = await evaluate(async () => {
+                return document.title;
+            });
+            expect(evaluationResult).to.equal(testName);
+        });
+
+    });
+
+});
