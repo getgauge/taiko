73f086a72bc1c4f4204b224c7c50ca74e5655e3a
BugDiver <vinaysh@thoughtworks.com>
2019-09-03 12:46:05 +0530

    Throwing error if element to click is disabled #782

--- test/unit-tests/click.test.js
+++ test/unit-tests/click.test.js
@@ -9,6 +9,7 @@ let {
   goto,
   text,
   below,
+  button,
   setConfig,
 } = require('../../lib/taiko');
 let {
@@ -159,6 +160,32 @@ describe(test_name, () => {
     it('should throw error', async () => {
       await expect(click('Click me')).to.be.rejectedWith(
         'Element matching text "Click me" is covered by other element',
+      );
+    });
+  });
+
+  describe('With element disabled', () => {
+    before(async () => {
+      let innerHtml = `
+        <body>
+            <button type="button" disabled>Click Me!</button>
+        </body>`;
+      overlayFilePath = createHtml(
+        innerHtml,
+        `${test_name}-disabled`,
+      );
+      await goto(overlayFilePath);
+      setConfig({ waitForNavigation: false });
+    });
+
+    after(() => {
+      setConfig({ waitForNavigation: true });
+      removeFile(overlayFilePath);
+    });
+
+    it('should throw error if element is disabled', async () => {
+      await expect(click(button('Click me'))).to.be.rejectedWith(
+        'Button with label Click me is disabled',
       );
     });
   });
