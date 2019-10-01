4aa423fae783d8be07387120da7773606dc50db8
BugDiver <vinaysh@thoughtworks.com>
2019-10-01 17:30:45 +0530

    Throwing error from switchTo if no URL given #848

---
+++ test/unit-tests/switchTo.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+let chaiAsPromised = require('chai-as-promised');
+const rewire = require('rewire');
+const taiko = rewire('../../lib/taiko');
+chai.use(chaiAsPromised);
+
+describe('switchTo', () => {
+    before(async () => {
+        taiko.__set__('validate', () => { });
+    });
+
+    it('should throw error if no url specified', async () => {
+        await expect(taiko.switchTo()).to.eventually.rejectedWith(TypeError);
+    });
+});
