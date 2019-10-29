6bd71730201d5f5e64bf08f793cb932af77ef554
Sai <saikrishna321@yahoo.com>
2019-10-29 23:13:11 +0000

    added feature to setTimeZone

--- test/functional-tests/tests/assert.js
+++ test/functional-tests/tests/assert.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 var _selectors = require('./selectors');
 
-const { title, text, textBox, toLeftOf } = require('taiko');
+const { title, text, textBox, toLeftOf, evaluate } = require('taiko');
 
 step('Assert title to be <userTitle>', async function(userTitle) {
   assert.ok((await title()).includes(userTitle));
@@ -41,3 +41,13 @@ step(
     assert.equal(valueis, 0);
   },
 );
+
+step('Assert page has set timezome', async function() {
+  const getTime = await evaluate(() => {
+    return date.toString();
+  });
+  assert.equal(
+    getTime,
+    'Sat Nov 19 2016 13:12:34 GMT-0500 (Eastern Standard Time)',
+  );
+});
