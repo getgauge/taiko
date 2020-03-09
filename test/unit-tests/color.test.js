2837052bb1971365fe7d00af87e687f1fd33bb59
Sai <saikrishna321@yahoo.com>
2020-03-09 20:32:59 +0000

    fixed review comments :smile:

--- test/unit-tests/color.test.js
+++ test/unit-tests/color.test.js
@@ -48,8 +48,30 @@ describe('Color picker test', () => {
     expect(await color({ id: 'head' }).value()).to.be.equal('#4903fc');
   });
 
+  it('Set color picker without hash', async () => {
+    await color({ id: 'head' }).select('4903fc');
+    expect(await color({ id: 'head' }).value()).to.be.equal('#4903fc');
+  });
+
   it('Set color picker with proximity selectors', async () => {
     await color(toLeftOf('Head')).select('#490333');
     expect(await color(toLeftOf('Head')).value()).to.be.equal('#490333');
+  });
+
+  it('Set color picker with rgb()', async () => {
+    await color(toLeftOf('Head')).select('rgb(69, 35, 30)');
+    expect(await color(toLeftOf('Head')).value()).to.be.equal('#45231e');
+  });
+
+  it('Should fail when color picker is set with invalid rgb()', async () => {
+    await expect(color(toLeftOf('Head')).select('rgb(69,30)')).to.be.rejectedWith(
+      'The color code rgb(69,30) is invalid. Please pass a valid HTML color code.',
+    );
+  });
+
+  it('Should fail when color picker is set with invalid hex value', async () => {
+    await expect(color(toLeftOf('Head')).select('#3233333')).to.be.rejectedWith(
+      'The color code #3233333 is invalid. Please pass a valid HTML color code.',
+    );
   });
 });
