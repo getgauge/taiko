919f776de73185ab4c497916d4244448d9e3d715
Nivedha <nivedhasenthil@gmail.com>
2020-03-24 17:31:00 +0530

    Fix switchTo to accept regex (#1151)
    * #1147 Fix switchTo to accept regex
    
    - update empty string check
    - add tests for regex switchTo
    - update wait for readystate to timeout on navigation timeout instead of default.
    
    * Bump up version to 1.0.7
    
    * Revert "Bump up version to 1.0.7"
    
    This reverts commit 3f0628aab5767697fdf3a4db925757d4aa479c75.

--- test/unit-tests/switchTo.test.js
+++ test/unit-tests/switchTo.test.js
@@ -6,8 +6,13 @@ const taiko = rewire('../../lib/taiko');
 chai.use(chaiAsPromised);
 
 describe('switchTo', () => {
+  let argument;
   before(async () => {
     taiko.__set__('validate', () => {});
+    taiko.__set__('targetHandler.getCriTargets', arg => {
+      argument = arg;
+      return { matching: [] };
+    });
   });
 
   it('should throw error if no url specified', async () => {
@@ -24,5 +29,12 @@ describe('switchTo', () => {
     await expect(taiko.switchTo('  ')).to.eventually.rejectedWith(
       'Cannot switch to tab or window. Hint: The targetUrl is empty. Please use a valid string or regex',
     );
+  });
+
+  it('should accept regex and call targetHandler with RegExp', async () => {
+    await expect(taiko.switchTo(/http(s):\/\/www.google.com/)).to.eventually.rejectedWith(
+      'No tab(s) matching /http(s):\\/\\/www.google.com/ found',
+    );
+    await expect(argument).to.deep.equal(new RegExp(/http(s):\/\/www.google.com/));
   });
 });
