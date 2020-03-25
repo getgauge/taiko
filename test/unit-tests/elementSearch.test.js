6bf7236e7d721b36ddd6c85bb2e32b2fc5a3715e
Dharmendra Singh <dharmenn@thoughtworks.com>
2020-03-25 17:40:21 +0530

    Websocket issue (#1115)
    * Handle WebSocket is not open: readyState 3 error
    
    * Pass openBrowserArgs to openBrowser
    
    * Check whether chrome process is alive or not before handling WebSocket is not open: readyState 3
    
    * Reset client related refrences when browser crashes.
    
    * Handle <WebSocket is not open: readyState 3> error in one place
    
    * Refactored waitUntil helper.
    
         - Throw actual error on time out.
         - Avoid waiting on browser process crash error.
    
    * bubble-up non timeout error.
    
    * Increase mocha timeout to 6000 for verbose reporter test script
    
    * Increase retry timeout for waitUntil tests.
    
    * Fix openBrowser test
    
    * Fix waitUntil helper test
    
    * Handle chrome process exited error
    
    * Create a wrapper around cdp api call to handle timeout.
    
    * Revert "Create a wrapper around cdp api call to handle timeout."
    
    This reverts commit 4309c9501f8031b6a532c18e7555d773e9ab1a4f.
    
    * Resolve promise when browser process is already killed
    
    * Fixed browserAction FT
    
    * Revert "Fixed browserAction FT"
    
    This reverts commit 809541393e1614a5c5f816c003d21cdde1e8d12c.
    
    * Create a wrapper around cdp api call to handle timeout.
    
    * Find proximity elements rects when needed.
    
    * Create proxies for missing CDP domain
    
    Co-authored-by: Vinay Shankar Shukla <vinaysh@thoughtworks.com>
    Co-authored-by: Nivedha <nivedhasenthil@gmail.com>
    Signed-off-by: Sven Vowé <sven.vowe.ext@dwpbank.de>

--- test/unit-tests/elementSearch.test.js
+++ test/unit-tests/elementSearch.test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const rewire = require('rewire');
 const Element = rewire('../../lib/elements/element');
 const elementSearch = rewire('../../lib/elementSearch');
+const { getIfExists } = elementSearch;
 class DomRects {}
 const TEXT_NODE = 3;
 describe('Element Search', () => {
@@ -60,6 +61,68 @@ describe('Element Search', () => {
       const visibleNodeIds = createElement([23, 89]);
       const elementsToFilter = createElement([23, 68, 89]);
       expect(await filterVisibleNodes(elementsToFilter)).to.eql(visibleNodeIds);
+    });
+  });
+  describe('getIfExists', () => {
+    let findElementCallCount,
+      originalWaitUntil = elementSearch.__get__('waitUntil');
+    const findElement = async () => {
+      findElementCallCount++;
+      return [{ id: 23 }];
+    };
+    beforeEach(() => {
+      findElementCallCount = 0;
+      elementSearch.__set__('waitUntil', async condition => {
+        await condition();
+      });
+    });
+
+    it('should wait and find elements', async () => {
+      let elements = await getIfExists(findElement, 'Element description')(null, 10, 10);
+
+      expect(elements).to.be.deep.equal([{ id: 23, description: 'Element description' }]);
+      expect(findElementCallCount).to.be.equal(1);
+    });
+
+    it('should try to find element after wait on unsuccessful finding', async () => {
+      let findElement = async () => {
+        findElementCallCount++;
+        let elements = findElementCallCount == 2 ? [{ id: 23 }] : [];
+        return elements;
+      };
+      let elements = await getIfExists(findElement, 'Element description')(null, 10, 10);
+
+      expect(elements).to.be.deep.equal([{ id: 23, description: 'Element description' }]);
+      expect(findElementCallCount).to.be.equal(2);
+    });
+
+    it('should add custom behaviour to elements', async () => {
+      let elements = await getIfExists(findElement, 'Element description', { exists: () => true })(
+        null,
+        10,
+        10,
+      );
+      const element = elements[0];
+      expect('exists' in element).to.be.true;
+      expect(element.exists()).to.be.true;
+    });
+
+    it('should handle wait timeout error', async () => {
+      elementSearch.__set__('waitUntil', originalWaitUntil);
+      let elements = await getIfExists(() => [], 'Element description')(null, 10, 10);
+
+      expect(elements.length).to.be.deep.equal(0);
+    });
+
+    it('should not handle non wait timeout error', async () => {
+      elementSearch.__set__('waitUntil', originalWaitUntil);
+      let elementFinder = await getIfExists(() => {
+        throw new Error('Non wait timeout error.');
+      }, 'Element description');
+
+      await expect(elementFinder(null, 10, 10)).to.be.eventually.rejectedWith(
+        'Non wait timeout error.',
+      );
     });
   });
 });
