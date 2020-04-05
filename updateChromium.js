b7aedd0dee41d512c1aa02ed564fda0896780509
Dharmendra Singh <dharmenn@thoughtworks.com>
2020-04-06 03:29:13 +0530

    Select latest revision available for all supported platform. #1119
    Signed-off-by: Dharmendra Singh <dharmenn@thoughtworks.com>

--- updateChromium.js
+++ updateChromium.js
@@ -1,54 +1,74 @@
 const https = require('https');
-const { writeFileSync } = require('fs');
-const { supportedPlatforms } = require('./lib/browserFetcher');
+const { writeFileSync, readFileSync } = require('fs');
+const { execSync } = require('child_process');
+const BrowserFetcher = require('./lib/browserFetcher');
+const supportedPlatforms = BrowserFetcher.supportedPlatforms;
 
-function getStableVersionInfo(chromiumReleases) {
-  let chromiumStableReleases = chromiumReleases
+async function checkAvailableRevision(revision, browserFetcher) {
+  let results = await Promise.all(supportedPlatforms.map((platform) => browserFetcher.canDownload(revision, platform)));
+  if (results.includes(false)) {
+    return await checkAvailableRevision(--revision, browserFetcher);
+  } else {
+    return revision;
+  };
+}
+
+async function findLatestCommonRevision(chromiumReleases) {
+  let latestRevisions = chromiumReleases
     .filter((release) => supportedPlatforms.includes(release.os))
     .map((release) => {
-      release.versions = release.versions.filter((version) => version.channel === 'stable');
-      return release;
+      let versions = release.versions.map((version) => parseInt(version.branch_base_position));
+      return Math.max(...versions);
     });
-  let { branch_base_position: revision, version } = chromiumStableReleases[0].versions[0];
-  let hasRevisionForAllPlatform = chromiumStableReleases.slice(1).every((stableRelease) => {
-    let versioInfp = stableRelease.versions[0];
-    return versioInfp.branch_base_position === revision;
-  });
-  if (!hasRevisionForAllPlatform) {
-    console.error(`The stable release revision(${revision}) does  ot exist for all platforms.`);
-    process.exit(1);
-  }
-  return { revision, version };
+  return await checkAvailableRevision(Math.min(...latestRevisions), new BrowserFetcher());
 }
 
-function updatePackageJSON({ revision, version }) {
-  let packageJSON = require('./package.json');
-  if (parseInt(packageJSON.taiko.chromium_revision) < parseInt(revision)) {
-    packageJSON.taiko.chromium_revision = revision;
-    packageJSON.taiko.chromium_version = version;
-    writeFileSync('./package.json', `${JSON.stringify(packageJSON, null, '  ')}\n`);
+function updatePackageJSON(key, value) {
+  let packageJSON = JSON.parse(readFileSync('./package.json', 'utf-8'));
+  if (key === 'revision') {
+    packageJSON.taiko.chromium_revision = `${value}`;
   } else {
-    console.log(
-      `Skipp updating packag.json as current chromium revision(${packageJSON.taiko.chromium_revision}) is similar or greater than available revision(${revision}).`,
-    );
+    packageJSON.taiko.chromium_version = `${value}`;
   }
+  writeFileSync('./package.json', `${JSON.stringify(packageJSON, null, '  ')}\n`);
 }
 
-function main() {
-  https
-    .get('https://omahaproxy.appspot.com/all.json', (res) => {
-      let data = '';
-      res.on('data', (datum) => {
-        data += datum;
-      });
-      res.on('end', () => {
-        let versionInfo = getStableVersionInfo(JSON.parse(data));
-        updatePackageJSON(versionInfo);
-      });
-    })
-    .on('error', (e) => {
-      console.error(e);
-    });
+async function getChromeReleasesInfo() {
+  return new Promise((resolve, reject) => {
+    https
+      .get('https://omahaproxy.appspot.com/all.json', (res) => {
+        let data = '';
+        res.on('data', (datum) => {
+          data += datum;
+        });
+        res.on('end', () => {
+          resolve(data);
+        });
+      })
+      .on('error', (e) => {
+        reject(e);
+      })
+  });
+}
+
+async function main() {
+  let releasesInfo = await getChromeReleasesInfo();
+  let revision = await findLatestCommonRevision(JSON.parse(releasesInfo));
+  let {chromium_revision} = require('./package.json').taiko;
+  if( chromium_revision >= revision ) {
+      console.log(
+      `Skipping updating package.json as current chromium revision(${chromium_revision}) is similar or greater than available revision(${revision}).`,
+    );
+    return;
+  }
+  updatePackageJSON('revision', revision);
+  execSync('node ./lib/install.js');
+  let browserFetcher = new BrowserFetcher();
+  let {executablePath} = browserFetcher.revisionInfo(revision);
+  let out = execSync(`${executablePath} --version`);
+  let versionInfo = out.toString();
+  let version = versionInfo.replace(/[^0-9\.]/g, '');
+  updatePackageJSON('version', version);
 }
 
 main();
