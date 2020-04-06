02f5f439322a6fa1de290063340d5e470fdd5a94
NivedhaSenthil <nivedhasenthil@gmail.com>
2020-04-06 16:10:06 +0530

    #818 fix closeWindow
    Signed-off-by: NivedhaSenthil <nivedhasenthil@gmail.com>

--- test/unit-tests/browserContext.test.js
+++ test/unit-tests/browserContext.test.js
@@ -14,7 +14,7 @@ let { isIncognito, getBrowserContexts } = require('../../lib/browserContext');
 
 let { createHtml, removeFile, openBrowserArgs, resetConfig } = require('./test-util');
 
-xdescribe('Browser Context', () => {
+describe('Browser Context', () => {
   let url1, url2;
   before(async () => {
     await openBrowser(openBrowserArgs);
