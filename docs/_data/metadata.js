c68e1bec9f09b1a6deb941c78bf296df9a8dba82
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-06-30 17:55:46 +0100

    Fetch metadata from Taiko
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

---
+++ docs/_data/metadata.js
@@ -0,0 +1,5 @@
+const { metadata } = require('../../lib/taiko');
+
+module.exports = function() {
+    return metadata;
+}
--- docs/api/reference.md
+++
@@ -1,6 +0,0 @@
----
-layout: page.njk
----
-{%- for api in apis %}
-* [{{ api.name}}](/api/{{ api.name }})
-{% endfor -%}
