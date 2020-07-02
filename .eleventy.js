4848fec436589be99935677698c517bd73e4105c
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-07-02 12:05:46 +0100

    Read api data from documentation source. Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,13 @@
 const tocPlugin = require('eleventy-plugin-toc');
 const markdownIt = require("markdown-it");
- 
-module.exports = function(eleventyConfig) {
-  // Output directory: _site
+
+module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("assets");
 
   // This filter is used for deriving the chapter names
   // from data in chapters.json
-  eleventyConfig.addFilter('heading', function(str){
+  eleventyConfig.addFilter('heading', function (str) {
     return str.replace(/[_;\\/:*?\"<>|&']/g, " ");
   });
 
@@ -16,16 +15,23 @@ module.exports = function(eleventyConfig) {
   // and displaying chapters in a page
   let markdownItAnchor = require("markdown-it-anchor");
   let mdIt = markdownIt({
-		html: true,
-		linkify: true
-	})
-	.use(markdownItAnchor, {
-		permalink: true,
-		permalinkBefore: false,
-		permalinkClass: "direct-link",
-		permalinkSymbol: "#",
-		level: [1,2]
-	});
-	eleventyConfig.setLibrary("md", mdIt);
+      html: true,
+      linkify: true
+    })
+    .use(markdownItAnchor, {
+      permalink: true,
+      permalinkBefore: false,
+      permalinkClass: "direct-link",
+      permalinkSymbol: "#",
+      level: [1, 2]
+    });
+  eleventyConfig.setLibrary("md", mdIt);
   eleventyConfig.addPlugin(tocPlugin);
+
+  return {
+    dir: {
+      input: 'docs',
+      output: 'docs/_site'
+    }
+  }
 };
