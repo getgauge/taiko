cfdd87a30b6edfb9c8720b8af713b87f45cec0a8
sriv <srikanth.ddit@gmail.com>
2020-08-20 18:28:21 +0530

    re-organize documentation, move it inline to types
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- docs/_data/apis.js
+++ docs/_data/apis.js
@@ -1,9 +1,13 @@
-const fs = require('fs')
+const fs = require('fs');
 
-module.exports = function(){
-    try {
-       return JSON.parse(fs.readFileSync('lib/api.json', 'utf-8')) 
-    } catch (error) {
-       throw new Error('Please run `npm run doc:api` to generate api metadata') 
-    }
-}
+module.exports = function () {
+  try {
+    var apis = { classes: [], functions: [] };
+    JSON.parse(fs.readFileSync('lib/api.json', 'utf-8')).forEach((x) =>
+      x.kind === 'class' ? apis.classes.push(x) : apis.functions.push(x),
+    );
+    return apis;
+  } catch (error) {
+    throw new Error('Please run `npm run doc:api` to generate api metadata');
+  }
+};
