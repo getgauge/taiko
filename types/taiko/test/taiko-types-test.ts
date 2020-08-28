128fa328ea7b6a49be83285c9985ee2d68ef993c
Filippo Maria Panelli <filippo.panelli@gmail.com>
2020-08-28 12:41:14 +0000

    Added type test from specs for openBrowser, closeBrowser
    The commit adds some tests for the functions openBrowser and closeBrowser.
    The tests are based on the examples provided in the official documentation.
    
    Signed-off-by: Filippo Maria Panelli <filippo.panelli@gmail.com>

--- types/taiko/test/taiko-types-test.ts
+++ types/taiko/test/taiko-types-test.ts
@@ -1,4 +1,30 @@
-import * as taiko from '../../taiko';
+import { openBrowser, closeBrowser } from '../../taiko';
+
+// ------------------------------------------
+// openBrowser
+// https://docs.taiko.dev/api/openbrowser
+// ------------------------------------------
+
+openBrowser(); // $ExpectType Promise<void>
+openBrowser({ headless: false }); // $ExpectType Promise<void>
+openBrowser({ args: ['--window-size=1440,900'] }); // $ExpectType Promise<void>
 
 // $ExpectType Promise<void>
-taiko.openBrowser();
+openBrowser({
+  args: [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+  ],
+});
+
+// ------------------------------------------
+// closeBrowser
+// https://docs.taiko.dev/api/closebrowser
+// ------------------------------------------
+
+closeBrowser(); // $ExpectType Promise<void>
+closeBrowser({}); // $ExpectError
