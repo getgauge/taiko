85107c4ba95a04915ba25481d6f893d3ab67cb68
BugDiver <vinaysh@thoughtworks.com>
2020-09-30 17:43:56 +0530

    Update gauge-ts version in plugins #1552
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>
    Signed-off-by: saikrishna321 <saikrishna321@yahoo.com>

--- test/functional-tests/tests/assert.ts
+++ test/functional-tests/tests/assert.ts
@@ -1,40 +1,40 @@
 const assert = require('assert');
 import { getElements } from './selectors';
-import { Step } from 'gauge-ts';
+import { Step, Table } from 'gauge-ts';
 import { title, text, textBox, toLeftOf, evaluate } from 'taiko';
 
 export default class Assert {
   @Step('Assert title to be <userTitle>')
-  public async assertTitle(userTitle) {
+  public async assertTitle(userTitle: string) {
     assert.ok((await title()).includes(userTitle));
   }
 
   @Step('Assert Exists <table>')
-  public async assertExists(table) {
+  public async assertExists(table: Table) {
     for (const element of getElements(table)) {
       assert.ok(await element.exists());
     }
   }
 
   @Step('assert text should be empty into <table>')
-  public async assertTextToBeEmpty(table) {
+  public async assertTextToBeEmpty(table: Table) {
     for (const element of getElements(table)) {
       assert.equal(await element.text(), '');
     }
   }
 
   @Step('Assert text <content> exists on the page.')
-  public async assertTextExists(content) {
+  public async assertTextExists(content: string) {
     assert.ok(await text(content).exists());
   }
 
   @Step('Assert text <content> does not exist')
-  public async assertTextDoesNotExists(content) {
+  public async assertTextDoesNotExists(content: string) {
     assert.ok(!(await text(content).exists(0, 0)));
   }
 
   @Step('Assert text <expectedText> exists on the textArea. <table>')
-  public async assertTextExistsOnTextArea(expectedText, table) {
+  public async assertTextExistsOnTextArea(expectedText: string, table: Table) {
     for (const element of getElements(table)) {
       const actualText = await textBox(toLeftOf(element)).value();
       assert.equal(actualText, expectedText.trim());
