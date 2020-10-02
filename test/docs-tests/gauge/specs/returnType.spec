b6cbdcb79f9e55e7f707c6f43911b02b3d58e201
Filippo Maria Panelli <filippo.panelli@gmail.com>
2020-10-02 21:46:48 +0000

    Fixing links in return-type docs for Object, Array, String, void
    Modified doc generator to provide correct links for JavaScript global
    objects Object, Array, String (Promise was already present).
    Also avoi generating a link for void.
    
    Signed-off-by: Filippo Maria Panelli <filippo.panelli@gmail.com>

--- test/docs-tests/gauge/specs/returnType.spec
+++ test/docs-tests/gauge/specs/returnType.spec
@@ -5,11 +5,11 @@
 |---------------------------------------|------------------------------|
 | functionReturningPromise              | Promise                      |
 | functionReturningPromiseOfObject      | Promise<Object>              |
-
+| functionReturningPromiseOfVoid        | Promise<void>                |
+| functionReturningArrayOfObjects       | Array<Object>                |
 ## <Function Name> should display <Return Type> after 'Returns'
 
 * Navigate to relative API reference page for <Function Name>
 * Assert text "Returns" exists on the page.
 * Assert text "Description of the return" exists below "Returns".
 * Assert text <Return Type> exists below "Returns".
-
--- test/docs-tests/jsdoc-inputs/returnTypes.js
+++ test/docs-tests/jsdoc-inputs/returnTypes.js
@@ -13,7 +13,6 @@ async function functionReturningPromiseOfObject(url) {
     a: url,
   }));
 }
-
 module.exports.functionReturningPromiseOfObject = functionReturningPromiseOfObject;
 
 /**
@@ -31,5 +30,55 @@ async function functionReturningPromise(url) {
     a: url,
   }));
 }
-
 module.exports.functionReturningPromise = functionReturningPromise;
+
+/**
+ * Test description of a test function.
+ *
+ * @example
+ * await functionReturningPromiseOfVoid('https://google.com')
+ *
+ * @param {string} url - URL to navigate page to.
+ *
+ * @returns {Promise<void>} - Description of the return
+ */
+async function functionReturningPromiseOfVoid(url) {
+  return new Promise(() => ({
+    a: url,
+  }));
+}
+module.exports.functionReturningPromiseOfVoid = functionReturningPromiseOfVoid;
+
+/**
+ * Test description of a test function.
+ *
+ * @example
+ * await functionReturningString('https://google.com')
+ *
+ * @param {string} url - URL to navigate page to.
+ *
+ * @returns {string} - Description of the return
+ */
+async function functionReturningString(url) {
+  return new Promise(() => ({
+    a: url,
+  }));
+}
+module.exports.functionReturningString = functionReturningString;
+
+/**
+ * Test description of a test function.
+ *
+ * @example
+ * await functionReturningArrayOfObjects('https://google.com')
+ *
+ * @param {string} url - URL to navigate page to.
+ *
+ * @returns {Object[]} - Description of the return
+ */
+async function functionReturningArrayOfObjects(url) {
+  return new Promise(() => ({
+    a: url,
+  }));
+}
+module.exports.functionReturningArrayOfObjects = functionReturningArrayOfObjects;
