375579f4987f52c35dca97fffb77a665eaec49b9
Ann Baiju <annbaiju@Anns-MacBook-Pro.local>
2021-06-29 10:50:09 +0530

    #1475 Add additional tests for paste API
    Signed-off-by: Simran Chawla <simran.chawla@thoughtworks.com>

--- test/unit-tests/paste.test.js
+++ test/unit-tests/paste.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const rewire = require('rewire');
 const EventEmitter = require('events').EventEmitter;
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -80,9 +81,160 @@ describe(test_name, () => {
   it('into focused element', async () => {
     const input = textBox({ id: 'focused' });
     const text = 'writing to focused input';
-    
+
     await paste(text);
 
     expect(await input.value()).to.equal(text);
+  });
+
+  it('should paste emoji char into focused element', async () => {
+    const input = textBox({ id: 'focused' });
+    const text = '🦘 🦡 🐨 🐯 🦁 🐮 🐷 🐽 🐸 writing to focused input';
+    await paste(text);
+    expect(await input.value()).to.equal(text);
+  });
+
+  it('into input field text', async () => {
+    const input = textBox({ id: 'text' });
+    const text = 'hello';
+    await paste(text, into(input));
+    expect(await input.value()).to.equal(text);
+  });
+
+  it('into input field by label', async () => {
+    const input = textBox({ id: 'labelled' });
+    const label = 'Labelled input:';
+    const text = 'hello';
+    await paste(text, into(label));
+    expect(await input.value()).to.equal(text);
+  });
+
+  it('should fail for readonly field', async () => {
+    const input = textBox({ id: 'readonly' });
+    const text = 'hello';
+    await expect(paste(text, into(input))).to.eventually.be.rejected;
+  });
+
+  it('should convert number to string value', async () => {
+    const input = textBox({ id: 'text' });
+    const number = 12345;
+    await expect(paste(number, into(input))).not.to.eventually.be.rejected;
+    expect(await input.value()).to.equal(number.toString());
+  });
+
+  it('should convert null to empty string value', async () => {
+    const input = textBox({ id: 'text' });
+    const empty = null;
+    await expect(paste(empty, into(input))).not.to.eventually.be.rejected;
+    expect(await input.value()).to.equal('');
+  });
+
+  it('should convert undefined to empty string value', async () => {
+    const input = textBox({ id: 'text' });
+    const empty = undefined;
+    await expect(paste(empty, into(input))).not.to.eventually.be.rejected;
+    expect(await input.value()).to.equal('');
+  });
+
+  it('should wait for element to be writable when selector is provided', async () => {
+    const input = textBox({ id: 'disabled' });
+    const text = 'Taiko can wait for element to be writable.';
+    await paste(text, into(input));
+    expect(await input.value()).to.equal(text);
+  });
+
+  it('should paste into shadow dom element', async () => {
+    const input = textBox({ id: 'shadow' });
+    const text = 'Shadow text updated';
+    await paste(text, into(input));
+    expect(await input.value()).to.equal(text);
+  });
+
+  describe('write test on multiple similar elements', () => {
+    before(async () => {
+      let innerHtml = `
+        <input type='text' id='text' readonly />
+        <input type='text' id='text' />
+        <input type='text' id='readonly' readonly />
+        <input type='text' id='readonly' readonly />
+      `;
+      filePath = createHtml(innerHtml, test_name);
+    });
+
+    after(async () => {
+      removeFile(filePath);
+    });
+
+    it('should paste into first writable element', async () => {
+      const input = textBox({ id: 'text' });
+      const text = 'hello';
+      await expect(paste(text, into(input))).not.to.eventually.be.rejected;
+    });
+
+    it('should reject if no element is writable', async () => {
+      const input = textBox({ id: 'readonly' });
+      const text = 'hello';
+      await expect(paste(text, into(input))).to.eventually.be.rejectedWith(
+        'TextBox[id="readonly"]is not writable',
+      );
+    });
+  });
+});
+
+// separated from the rest because taiko needs to get rewired
+describe('paste with hideText option', () => {
+  let filePath, actualEmmiter, taiko;
+  let emitter = new EventEmitter();
+
+  let validateEmitterEvent = function (event, expectedText) {
+    return new Promise((resolve) => {
+      emitter.on(event, (res) => {
+        expect(res).to.be.equal(expectedText);
+        resolve();
+      });
+    });
+  };
+
+  before(async () => {
+    taiko = rewire('../../lib/taiko.js');
+    actualEmmiter = taiko.__get__('descEvent');
+
+    taiko.__set__('descEvent', emitter);
+
+    filePath = createHtml(innerHtml, test_name);
+    await taiko.openBrowser(openBrowserArgs);
+    await taiko.goto(filePath);
+    taiko.setConfig({
+      waitForNavigation: false,
+      retryTimeout: 100,
+      retryInterval: 10,
+    });
+  });
+
+  after(async () => {
+    removeFile(filePath);
+    taiko.closeBrowser();
+    taiko.__set__('descEvent', actualEmmiter);
+  });
+
+  afterEach(() => {
+    emitter.removeAllListeners();
+  });
+
+  it.skip('should mask the text when pasting to focused element', async () => {
+    let validatePromise = validateEmitterEvent('success', 'Pasted ***** into the focused element.');
+    await taiko.paste('pasting to focused input', { hideText: true });
+    await validatePromise;
+  });
+
+  it('should mask the text when pasting into a selected element', async () => {
+    let validatePromise = validateEmitterEvent(
+      'success',
+      'Pasted ***** into the textBox[id="text"]',
+    );
+    const input = taiko.textBox({ id: 'text' });
+    const text = 'hello';
+    await taiko.paste(text, taiko.into(input), { hideText: true });
+    await validatePromise;
   });
 });
