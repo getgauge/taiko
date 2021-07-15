6de0160291e2e2362075268702548a54af92e91b
Siddharth <44291368+Siddharth-Tumre@users.noreply.github.com>
2021-07-15 08:43:59 +0530

    Error while drag n drop - ProtocolError: Invalid parameters #2149 (#2168)
    * Refactor code to throw an error when we perform dragAndDrop on hidden elements
    
    Signed-off-by: Siddharth Tumre <siddharthtumre2@hotmail.com>
    
    * Add unit test for drag and drop
    
    Signed-off-by: Siddharth Tumre <siddharthtumre2@hotmail.com>
    
    * Refactor test names
    
    Signed-off-by: Siddharth Tumre <siddharthtumre2@hotmail.com>
    
    * Refactor dragAndDrop to check for the visibility of destination if it an element or selector
    
    Signed-off-by: Siddharth Tumre <siddharth.tumre@thoughtworks.com>
    
    * Remove. unnecessary checks for destination element in dragAndDrop
    
    Signed-off-by: Siddharth Tumre <siddharth.tumre@thoughtworks.com>
    
    Co-authored-by: Siddharth Tumre <siddharthtumre2@hotmail.com>
    Co-authored-by: Nivedha <nivedhasenthil@gmail.com>
    Co-authored-by: Siddharth Tumre <siddharth.tumre@thoughtworks.com>

---
+++ test/unit-tests/dragAndDrop.test.js
@@ -0,0 +1,69 @@
+const { descEvent } = require('../../lib/helper');
+
+let { openBrowser, goto, dragAndDrop, setConfig, closeBrowser, $ } = require('../../lib/taiko');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+let { createHtml, removeFile, openBrowserArgs, resetConfig } = require('./test-util');
+const test_name = 'DragAndDrop';
+
+describe(test_name, () => {
+  let filePath;
+
+  let validateEmitterEvent = function (event, expectedText) {
+    return new Promise((resolve) => {
+      descEvent.once(event, (eventData) => {
+        expect(eventData).to.be.equal(expectedText);
+        resolve();
+      });
+    });
+  };
+  before(async () => {
+    let innerHtml = `<div id="columns">
+        <div class="column" id="column-a" draggable="true"><header>A</header></div>
+        <div class="column" id="column-b" draggable="true" hidden><header>B</header></div>
+        </div>`;
+
+    filePath = createHtml(innerHtml, test_name);
+    await openBrowser(openBrowserArgs);
+    await goto(filePath);
+    setConfig({
+      waitForNavigation: false,
+      retryTimeout: 10,
+      retryInterval: 10,
+    });
+  });
+
+  after(async () => {
+    resetConfig();
+    await closeBrowser();
+    removeFile(filePath);
+  });
+
+  describe('Drag and Drop test', () => {
+    it('Should throw an error if either source or destination is not present', async () => {
+      try {
+        expect(await dragAndDrop($('#column-a'), $('#column-c')));
+      } catch (err) {
+        expect(err.message).to.equal('CustomSelector with query #column-c  not found');
+      }
+    });
+
+    it('Should throw an error if either source or destination are hidden', async () => {
+      try {
+        expect(await dragAndDrop($('#column-a'), $('#column-b')));
+      } catch (err) {
+        expect(err.message).to.equal('CustomSelector with query #column-b is not visible');
+      }
+    });
+
+    it('Should throw an error when we try to drag and drop hidden items forcefully', async () => {
+      try {
+        expect(await dragAndDrop($('#column-a'), $('#column-b'), { force: true }));
+      } catch (err) {
+        expect(err.message).to.equal('Taiko cannot drag hidden elements');
+      }
+    });
+  });
+});
