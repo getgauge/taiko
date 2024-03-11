3fb26e261be6044b7dd9d7fc8c896d5b5f0e9b9a
Alexlesch98 <160536953+Alexlesch98@users.noreply.github.com>
2024-03-11 11:32:57 +0100

    Update .eleventy.js
    In this refactored version:
    
    1. I've grouped the required modules at the beginning for better clarity.
    2. Each configuration or plugin addition is separated with comments for easier understanding.
    3. The Markdown rendering configuration is now encapsulated in a variable (mdIt) for better modularity.
    4. The conditional check in the eleventyConfig.addFilter('markdown', ...) function is simplified for readability.

--- .eleventy.js
+++ .eleventy.js
@@ -1,45 +1,49 @@
 const tocPlugin = require('eleventy-plugin-toc');
-const markdownIt = require("markdown-it");
-const CleanCSS = require("clean-css");
+const markdownIt = require('markdown-it');
+const CleanCSS = require('clean-css');
+const markdownItAnchor = require('markdown-it-anchor');
 
 module.exports = function (eleventyConfig) {
+  // Passthrough copy for assets
+  eleventyConfig.addPassthroughCopy('docs/assets');
 
-  eleventyConfig.addPassthroughCopy("docs/assets");
-  eleventyConfig.addFilter("cssmin", function(code) {
+  // CSS minification filter
+  eleventyConfig.addFilter('cssmin', function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
-  // This filter is used for deriving the chapter names
-  // from data in chapters.json
+
+  // Heading filter for deriving chapter names
   eleventyConfig.addFilter('heading', function (str) {
-    return str.replace(/[_;\\/:*?\"<>|&']/g, " ");
+    return str.replace(/[_;\\/:*?\"<>|&']/g, ' ');
   });
 
-  // Filter for linking to section headers
-  // and displaying chapters in a page
-  let markdownItAnchor = require("markdown-it-anchor");
-  let mdIt = markdownIt({
-      html: true,
-      linkify: true
-    })
-    .use(markdownItAnchor, {
-      permalink: true,
-      permalinkBefore: false,
-      permalinkClass: "direct-link",
-      permalinkSymbol: "#",
-      level: [1, 2]
-    });
-  eleventyConfig.addFilter("markdown", function(code) {
-      if(code) {
-        return mdIt.render(code);
-      }
+  // Markdown rendering filter
+  const mdIt = markdownIt({
+    html: true,
+    linkify: true,
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkBefore: false,
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+    level: [1, 2],
   });
-  eleventyConfig.setLibrary("md", mdIt);
+
+  eleventyConfig.addFilter('markdown', function (code) {
+    return code ? mdIt.render(code) : '';
+  });
+
+  // Set Markdown library
+  eleventyConfig.setLibrary('md', mdIt);
+
+  // Add TOC plugin
   eleventyConfig.addPlugin(tocPlugin);
 
+  // Configuration for Eleventy
   return {
     dir: {
       input: 'docs',
-      output: 'docs/_site'
-    }
-  }
+      output: 'docs/_site',
+    },
+  };
 };
