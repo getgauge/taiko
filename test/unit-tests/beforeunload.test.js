ca255fc2eb3f926f0d3212b1aafbad1cff3507b9
Nivedha <nivedhasenthil@gmail.com>
2024-05-24 14:00:43 +0530

    add back beforeunload
    - fix the test by updating the script
    
    Signed-off-by: Nivedha <nivedhasenthil@gmail.com>

--- test/unit-tests/beforeunload.test.js
+++ test/unit-tests/beforeunload.test.js
@@ -16,9 +16,7 @@ let {
 let { createHtml, removeFile, openBrowserArgs } = require('./test-util');
 const test_name = 'beforeunload';
 
-// TODO: Skip this test and remove the unload functionality
-// Refer:https://developer.chrome.com/docs/web-platform/deprecating-unload
-xdescribe(test_name, () => {
+describe(test_name, () => {
   let filePath, filePath1;
   let called = false;
   before(async () => {
@@ -31,7 +29,9 @@ xdescribe(test_name, () => {
         window.addEventListener('beforeunload', function (e) {
           let newValue = document.getElementById('name').value;
           if( oldValue != newValue) {
-            e.returnValue = '';
+            event.preventDefault();
+            // Legacy support for older browsers.
+            return (event.returnValue = true);
           }
         });
     </script>
